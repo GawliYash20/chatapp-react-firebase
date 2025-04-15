d87f82ae498177e95be75b1d258ec4db49d79433
GawliYash20 <yash.gawli18@gmail.com>
2025-04-15 11:08:45 +0530

    initial commit

---
+++ src/components/details/Detail.jsx
@@ -0,0 +1,54 @@
+import "./detail.css";
+
+const Detail = () => {
+  return (
+    <div className="detail">
+      <div className="user">
+        <img src="./avatar.png" alt="" />
+        <h2>Jane Doe</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+      </div>
+      <div className="info">
+        <div className="options">
+          <div className="title">
+            <span>Chat Settings</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="options">
+          <div className="title">
+            <span>Privacy & Help</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <div className="options">
+          <div className="title">
+            <span>Shared Photos</span>
+            <img src="./arrowDown.png" alt="" />
+          </div>
+          <div className="photos">
+            <div className="photoItem">
+              <div className="photoDetail">
+                <img
+                  src="https://images.pexels.com/photos/31049335/pexels-photo-31049335/free-photo-of-majestic-longhorn-cow-grazing-in-wisconsin-field.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                  alt=""
+                />
+                <span>photo_2024_2.png</span>
+              </div>
+              <img src="./download.png" alt="" className="icon"/>
+            </div>
+          </div>
+        </div>
+        <div className="options">
+          <div className="title">
+            <span>Shared files</span>
+            <img src="./arrowUp.png" alt="" />
+          </div>
+        </div>
+        <button>Block User</button>
+      </div>
+    </div>
+  );
+};
+
+export default Detail;
